916c6a458a7ca9cdb1b6eaef4d0dbaf169f831d3
ugansinghmaharjan <uganmaharjan@gmail.com>
2026-02-03 10:09:47 +0545

    changed link location

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -283,7 +283,7 @@ export default function Navbar() {
                 >
                   {user?.role === "admin" && (
                     <Link
-                      href="/admin/products"
+                      href="/admin"
                       className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 hover:text-primary"
                     >
                       <Settings className="w-4 h-4 mr-2" />
